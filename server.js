4aebc498bc7b18c088664476c737ef83ce33875d
luminhphuc258 <luminhphuc258@gmail.com>
2025-10-19 02:25:24 -0500

    update music v13

--- server.js
+++ server.js
@@ -27,6 +27,11 @@ const port = process.env.PORT || 8080;
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 // ==== Middleware ====
+app.enable("trust proxy");
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
